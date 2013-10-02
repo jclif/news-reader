56ff69ac5f4dd2b125f849effefa672d83bfa8fb
AppAcademy Student <>
2013-10-02 09:11:16 -0400

    rails filesssssSSSss

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsReader::Application.config.secret_token = '495a2c1ca8eff8d4cbbd435b8deaeb5f545e449cde9d21237c8561f18998c3c1dac2e2da329da99fd39a27f7f5bf9ab437bf73b715a8e55bfc5d922dcd1ce8e3'
